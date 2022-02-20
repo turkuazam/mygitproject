eb628a7c7873c4c51e65de38058c0a2a1020b24b
turkuazam <ayyildizlimatbaa@gmail.com>
2022-02-20 19:50:23 +0300

    localden yzıyorum

--- src/mygitpackage/myGitClass.java
+++ src/mygitpackage/myGitClass.java
@@ -8,15 +8,12 @@ public class myGitClass {
 		System.out.println("Mey second commit");
 		System.out.println("My third commit");
 		System.out.println("My 4th commit");
-<<<<<<< HEAD
-			System.out.println("github'tan değişiklik yaptım");//github'tan çekilen satır
-		
+		System.out.println("github'tan değişiklik yaptım"); //github'tan çekilen satır
 		System.out.println("My 5th commit");
-		
-=======
-			System.out.println("github'tan deÄŸiÅŸiklik yaptÄ±m");//github'tan Ã§ekilen satÄ±r
-		System.out.println("github'ta  12.satÄ±rda deÄŸiÅŸiklik");
->>>>>>> refs/remotes/origin/master
+					
+			
+			System.out.println("localde  15.satÄ±rda deÄŸiÅŸiklik");
+
 	}
 
 }
